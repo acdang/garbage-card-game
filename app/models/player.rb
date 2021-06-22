f0c37936da2352e03e5fda2f6c161c7c41298839
Anh Dang <anhcdang@gmail.com>
2021-06-22 15:23:38 -0700

    Created pseudocode, edited player.rb

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,22 @@
 class Player < ActiveRecord::Base
     has_many :play_rounds
     has_many :rounds, through: :play_rounds
+
+    # display button: "Sign up"
+    # or something like "Press enter to sign up"
+
+    # want to get first and last name from user and create new Player instance with name
+    # class method for now...
+    def self.new_player
+        puts "Enter your first name:"
+        f_name = STDIN.gets.chomp # doesn't work
+        puts "Enter your last name:"
+        l_name = STDIN.gets.chomp
+
+        puts "Hello, #{first_name} #{last_name}"
+
+        # Player.create(first_name: f_name, last_name: Faker::Name.l_name, is_cpu: false)
+    end
+
+    # "Press enter to start game"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@ puts "Creating CPU players..."
 3.times do
     Player.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, is_cpu: true)
 end
+
+puts "CPU players created!"
