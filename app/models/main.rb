17f729e171a790806359282be710937a2616d418
Anh Dang <anhcdang@gmail.com>
2021-06-22 17:22:54 -0700

    create new user, check existing user

--- app/models/main.rb
+++ app/models/main.rb
@@ -5,59 +5,74 @@ require "tty-prompt"
 prompt = TTY::Prompt.new
 
 # creates new Player with user input name
+
+def get_first_name
+    prompt = TTY::Prompt.new
+    puts "\n"
+    prompt.ask("Enter your FIRST name:") do |q|
+        q.required  true
+        q.validate  /\A\w+\Z/
+        q.modify    :capitalize
+    end
+end
+
+def get_last_name
+    prompt = TTY::Prompt.new
+
+    prompt.ask("Enter your LAST name:") do |q|
+        q.required  true
+        q.validate  /\A\w+\Z/
+        q.modify    :capitalize
+    end
+end
+
 def sign_up
-    puts "Enter your first name:"
-    f_name = STDIN.gets.chomp
-    puts "Enter your last name:"
-    l_name = STDIN.gets.chomp
+    f_name = get_first_name()
+    l_name = get_last_name()
 
     puts "\n"
     puts "Hello, #{f_name} #{l_name}"
-    Player.new_player(f_name, l_name)
+    # Player.new_player(f_name, l_name)
 end
 
 # check if Player exists
 def check_player
-    puts "Enter your first name:"
-    f_name = STDIN.gets.chomp
-    puts "Enter your last name:"
-    l_name = STDIN.gets.chomp
+    f_name = get_first_name()
+    l_name = get_last_name()
+    
+    user_exists = false
 
     if Player.check_player(f_name, l_name)
         puts "\n"
         puts "Hello, #{f_name} #{l_name}"
+        user_exists = true
     else
         puts "\n"
-        puts "User does not exist"
+        puts "That user does not exist"
+        user_exists
     end
 end
 
 puts "Welcome to Garbage, a card game!"
 puts "\n"
 
-# puts "Would you like to know the rules? ('yes' or 'no')"
-
-    # puts "Are you a new player? 'yes' or 'no')"
-    # new_player = STDIN.gets.chomp
-
 new_player = prompt.yes?("Are you a new player?")
 
 # create new Player if yes
 if new_player
-    puts "\n"
-    puts "You are a new player!"
-
     sign_up()
 # check Player if no
-elsif !new_player
-    puts "\n"
-    puts "You are not a new player."
-
-    check_player()
 else
-    puts "\n"
-    puts "Not a valid response."
+    loop do
+        check_player()
+    break if check_player()
+    end
 end
 
 # DISPLAY STATS HERE
 
+# "Choose your opponent by entering their number"
+# 1. Opponent 1
+# 2. Opponent 2
+# 3. Opponent 3
+# [save this opponent to variable]
