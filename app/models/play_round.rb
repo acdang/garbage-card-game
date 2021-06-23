f735ca6ed85f0f5f24f105117893f2e082ce549c
Anh Dang <anhcdang@gmail.com>
2021-06-23 10:18:06 -0700

    update Player.new_lost_round & Player.new_won_round

--- app/models/play_round.rb
+++ app/models/play_round.rb
@@ -2,11 +2,11 @@ class PlayRound  < ActiveRecord::Base
     belongs_to :player
     belongs_to :round
 
-    def self.new_lost_round(round_id, player_id)
-        PlayRound.create(is_winner: false, round_id: round_id, player_id: player_id)
+    def self.new_lost_round
+        PlayRound.create(is_winner: false)
     end
 
-    def self.new_won_round(round_id, player_id)
-        PlayRound.create(is_winner: true, round_id: round_id, player_id: player_id)
+    def self.new_won_round
+        PlayRound.create(is_winner: true)
     end
 end 
