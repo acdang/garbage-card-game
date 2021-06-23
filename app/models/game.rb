134fbc07baf5d5fafdaba18a5a7531031ffcdc4a
Anh Dang <anhcdang@gmail.com>
2021-06-22 21:33:52 -0700

    edit #check_player, add Game.new_game & Game#finished

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,11 @@
 class Game < ActiveRecord::Base
     has_many :rounds
+
+    def self.new_game
+        Game.create(is_complete: false)
+    end
+
+    def finished
+        Game.update(self.id, "is_complete" => true)
+    end
 end
--- app/models/main.rb
+++ app/models/main.rb
@@ -24,53 +24,77 @@ def get_last_name
 end
 
 def sign_up
-    puts "Sign up:\n"
+    puts "---> SIGN UP\n"
 
     f_name = get_first_name()
     l_name = get_last_name()
 
     puts "\n"
-    puts "Hello, #{f_name} #{l_name}"
-    # Player.new_player(f_name, l_name)
+    registered = TTY::Prompt.new
+    registered.ok("Hello, #{f_name} #{l_name}!")
+    puts "\n"
+    # Player.new_player(f_name, l_name) # UNCOMMENT THIS LATER!!!!!!!!
 end
 
 # check if Player exists
 def check_player
-    puts "Log in:\n"
+    user_exists = false
 
-    f_name = get_first_name()
-    l_name = get_last_name()
+    while user_exists == false
+        puts "---> LOG IN\n"
 
-    if Player.check_player(f_name, l_name)
-        puts "\n"
-        puts "Hello, #{f_name} #{l_name}"
-        return true
-    else
-        puts "\n"
-        puts "That user does not exist"
-        return false
+        f_name = get_first_name()
+        l_name = get_last_name()
+
+        if Player.check_player(f_name, l_name)
+            puts "\n"
+            exist = TTY::Prompt.new
+            exist.ok("Hello, #{f_name} #{l_name}!")
+            puts "\n"
+            user_exists = true
+        else
+            puts "\n"
+            not_exist = TTY::Prompt.new
+            not_exist.error("That user does not exist.")
+            puts "\n"
+        end
     end
 end
+
+# Welcome ======================================================
 
 puts "Welcome to Garbage, a card game!"
 puts "\n"
 
+# Handle player ======================================================
+
 new_player = TTY::Prompt.new
-new_player.yes?("Are you a new player?") # change to select?
+# new_player.yes?("Are you a new player?") # change to select?
+options = [{name: "New Player", value: 1}, {name: "Returning Player", value: 2}]
+status = new_player.select("What's your status?", options)
+puts "\n"
 
 # create new Player if yes
-if new_player
+if status == 1
     sign_up()
 # check Player if no
 else
-    while !check_player()
-        heck_player()
-    end
+    check_player()
 end
+# Display stats ======================================================
+
+puts "STATS WILL GO HERE"
+puts "\n"
+
+# Create new game ======================================================
+continue = TTY::Prompt.new(active_color: :cyan)
+continue.keypress("---> Press enter to create a NEW GAME\n", keys: [:return])
+
+current_game = Game.new_game
 
 # Choose opponent ======================================================
 
-cpu_opponent = TTY::Prompt.new
+opponent_prompt = TTY::Prompt.new
 
 opponent1 = Player.first
 opponent2 = Player.second
@@ -81,7 +105,6 @@ choices = [
     {name: opponent2.full_name, value: opponent2},
     {name: opponent3.full_name, value: opponent3}
 ]
-cpu_opponent.select("Choose your opponent:", choices)
+cpu_opponent = opponent_prompt.select("Choose your opponent:", choices)
 
-# ======================================================
 
