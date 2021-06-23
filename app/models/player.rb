c48b0b2b0f7028236e1bb59d9aea6eb47fc90c4f
Anh Dang <anhcdang@gmail.com>
2021-06-23 11:13:36 -0700

    update Player#total_num_games, add Player#games & Player#total_num_completed_games

--- app/models/player.rb
+++ app/models/player.rb
@@ -15,28 +15,27 @@ class Player < ActiveRecord::Base
         "#{self.first_name} #{self.last_name}"
     end
 
-    # return total num of games a player has played
+    # return all games played by a player
+    def games
+        # get all game_ids from all player's rounds
+        game_ids = self.rounds.map {|round| round.game_id}.uniq
+
+        # return an array of all games played by player
+        Game.find(game_ids)
+    end
+
+    # return total num games a player has played (completed or not completed)
     # WILL SIMPLIFY
     def total_num_games
-        # get all player's PlayRounds
-        all_play_rounds = self.play_rounds #if nothing, return 0
-
-        # get all player's Round ids (uniq)
-        all_round_ids_uniq = all_play_rounds.map {|play_round| play_round.round_id}.uniq
-
-        #get Rounds from those ids
-        rounds = Round.find(all_round_ids_uniq)
-
-        # get game ids from those Rounds
-        all_game_ids_uniq = rounds.map {|round| round.game_id}.uniq
-
-        # get all games (uniq) -- returned in an array
-        all_games = Game.find(all_game_ids_uniq)
-
-        # return count of games
-        all_games.count
-
+        self.games.count
     end
+
+    def total_num_completed_games
+        self.games.select {|game| game.is_complete == true}.count
+    end
+
+
+
     # return total num of games a player has won
     def num_games_won
         # get all player's PlayRounds
