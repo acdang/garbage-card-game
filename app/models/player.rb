6c3f64931a2ae9dffd30af5cee9a4f6c92eb3c40
Anh Dang <anhcdang@gmail.com>
2021-06-23 08:11:46 -0700

    map out Player#total_games

--- app/models/player.rb
+++ app/models/player.rb
@@ -14,4 +14,21 @@ class Player < ActiveRecord::Base
     def full_name
         "#{self.first_name} #{self.last_name}"
     end
+
+    # return total num of games a player has played
+    # WILL SIMPLIFY
+    def total_games
+        # get all player's PlayRounds
+        all_play_rounds = self.play_rounds
+
+        # get all player's Round ids (uniq)
+        all_round_ids_uniq = all_play_rounds.map {|play_round| play_round.round_id}.uniq
+
+        # get all games (uniq) -- returned in an array
+        all_games = Game.find(all_round_ids_uniq)
+
+        # return count of games
+        all_games.count
+
+    end
 end
